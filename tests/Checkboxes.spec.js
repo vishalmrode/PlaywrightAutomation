318892623aeedf4c6604d358a662cf6b00031aab
vishalmrode <vishalmrode@outlook.com>
2024-07-29 18:50:18 +0530

    Used for loop to check and uncheck multiple boxes using for loop

--- tests/Checkboxes.spec.js
+++ tests/Checkboxes.spec.js
@@ -1,16 +1,44 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Handle Checkboxes',async({page})=>{
+test("Handle Checkboxes", async ({ page }) => {
+  await page.goto("https://demoqa.com/checkbox");
 
-    await page.goto ('https://demoqa.com/checkbox');
+  await page.getByLabel("Expand all").click();
+  await page.locator("//span[contains(text(),'Notes')]").check();
 
-    await page.getByLabel('Expand all').click();
-    await page.locator("//span[contains(text(),'Notes')]").check();
+  await expect(await page.locator("//span[contains(text(),'Notes')]").isChecked()).toBeTruthy();
+  await expect(await page.locator("//span[contains(text(),'WorkSpace')]").isChecked()).toBeFalsy();
+});
+
+test("Multiple checkboxes to be checked and unchecked again", async ({page}) => {
+  
+    // Multiple Checkboxes
+  const checkboxLocators = [
+    "//span[contains(text(),'Public')]", // Create an array of multiple checkboxes to be selected
+    "//span[contains(text(),'Private')]",
+    "//span[contains(text(),'Classified')]",
+    "//span[contains(text(),'General')]",
+    ];
+
+  // use for loop to select the checkboxes
+  for (const locator of checkboxLocators) {
     
-    await expect(await page.locator("//span[contains(text(),'Notes')]").isChecked()).toBeTruthy();
-    await expect(await page.locator("//span[contains(text(),'WorkSpace')]").isChecked()).toBeFalsy();
-    await page.waitForTimeout(1000);
+    // Select multiple checkboxes
+    await page.locator(locator).check();
+  }
+  await page.waitForTimeout(2000);
 
-}
 
-)
+
+  // use for loop to Unselect the checkboxes
+  for (const locator of checkboxLocators) {
+
+    // UnSelect multiple checkboxes which are already selcted
+
+    if (await page.locator(locator).isChecked);
+    {
+      await page.locator(locator).uncheck();
+    }
+  }
+  await page.waitForTimeout(1000);
+});
