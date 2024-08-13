4767916a3394be5ec699902246443affa17b5459
vishalmrode <vishalmrode@outlook.com>
2024-08-13 19:24:40 +0530

    Changed the priority of Test case, removed only keyword

--- tests/Screenshot.spec.js
+++ tests/Screenshot.spec.js
@@ -3,14 +3,17 @@ import { test, expect } from '@playwright/test';
 test('page screenshot', async ({ page }) => {
  await page.goto('https://demo.opencart.com/')
  await page.screenshot({ path:'tests/screenshots/'+Date.now()+'HomePage.png'})
+ await page.waitForTimeout(5000);
 });
 
 test('Full page screenshot', async ({ page }) => {
     await page.goto('https://demo.opencart.com/')
     await page.screenshot({ path:'tests/screenshots/'+Date.now()+'FullPage.png',fullPage:true})
+    await page.waitForTimeout(5000);
 });
 
-test.only('Element screenshot', async ({ page }) => {
+test('Element screenshot', async ({ page }) => {
     await page.goto('https://demo.opencart.com/')
     await page.locator('//*[@id="content"]/div[2]/div[1]/form/div').screenshot({ path:'tests/screenshots/'+Date.now()+'Macbook.png'})
+    await page.waitForTimeout(5000);
 });
