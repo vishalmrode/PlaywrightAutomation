6cfad188d1a07f085b1f54851926fdd41a77d819
vishalmrode <vishalmrode@outlook.com>
2024-09-05 16:49:12 +0530

    screenshot

--- tests/Screenshot.spec.js
+++ tests/Screenshot.spec.js
@@ -1,19 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('page screenshot', async ({ page }) => {
- await page.goto('https://demo.opencart.com/')
- await page.screenshot({ path:'tests/screenshots/'+Date.now()+'HomePage.png'})
- await page.waitForTimeout(5000);
+test.only("page screenshot", async ({ page }) => {
+  await page.goto("https://demo.opencart.com/");
+  await page.screenshot({
+    path: "tests/screenshots/" + "HomePage.png" + Date.now(),
+  });
+  await page.waitForTimeout(5000);
 });
 
-test('Full page screenshot', async ({ page }) => {
-    await page.goto('https://demo.opencart.com/')
-    await page.screenshot({ path:'tests/screenshots/'+Date.now()+'FullPage.png',fullPage:true})
-    await page.waitForTimeout(5000);
+test("Full page screenshot", async ({ page }) => {
+  await page.goto("https://demo.opencart.com/");
+  await page.screenshot({
+    path: "tests/screenshots/" + "FullPage.png" + Date.now(),
+    fullPage: true,
+  });
+  await page.waitForTimeout(5000);
 });
 
-test('Element screenshot', async ({ page }) => {
-    await page.goto('https://demo.opencart.com/')
-    await page.locator('//*[@id="content"]/div[2]/div[1]/form/div').screenshot({ path:'tests/screenshots/'+Date.now()+'Macbook.png'})
-    await page.waitForTimeout(5000);
+test("Element screenshot", async ({ page }) => {
+  await page.goto("https://demo.opencart.com/");
+  await page
+    .locator('//*[@id="content"]/div[2]/div[1]/form/div')
+    .screenshot({ path: "tests/screenshots/" + Date.now() + "Macbook.png" });
+  await page.waitForTimeout(5000);
 });
