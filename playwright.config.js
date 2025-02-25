d2fb41012ad9eb4091e1d9587ed5b4cc6141e5b6
vishalmrode <vishalmrode@outlook.com>
2025-02-25 16:27:01 +0530

    Removed snaps and fully parallel execution

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig({
   },
 
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -33,7 +33,7 @@ module.exports = defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    browserName: "chromium",
+    browserName: "firefox",
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
@@ -42,42 +42,42 @@ module.exports = defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
+  // projects: [
+  //   {
+  //     name: "chromium",
+  //     use: { ...devices["Desktop Chrome"] },
+  //   },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+  //   {
+  //     name: "firefox",
+  //     use: { ...devices["Desktop Firefox"] },
+  //   },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+  //   {
+  //     name: "webkit",
+  //     use: { ...devices["Desktop Safari"] },
+  //   },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+  //   /* Test against mobile viewports. */
+  //   // {
+  //   //   name: 'Mobile Chrome',
+  //   //   use: { ...devices['Pixel 5'] },
+  //   // },
+  //   // {
+  //   //   name: 'Mobile Safari',
+  //   //   use: { ...devices['iPhone 12'] },
+  //   // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+  //   /* Test against branded browsers. */
+  //   // {
+  //   //   name: 'Microsoft Edge',
+  //   //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+  //   // },
+  //   // {
+  //   //   name: 'Google Chrome',
+  //   //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+  //   // },
+  // ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,4 +1,4 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
 test("Browser context First Test case", async ({ browser }) => {
   const context = await browser.newContext();
@@ -8,8 +8,7 @@ test("Browser context First Test case", async ({ browser }) => {
 });
 
 test("Page context Test case", async ({ page }) => {
-  //   const context = await browser.newContext();
-  //   const page = await context.newPage();
-
   await page.goto("https://google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
 });
