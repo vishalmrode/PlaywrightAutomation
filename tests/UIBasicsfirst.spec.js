b1dee720f8a0dfb9900d3211e7a0fbd6395e9a5a
vishalmrode <vishalmrode@outlook.com>
2025-02-26 13:48:44 +0530

    Created new file for login page test

--- tests/UIBasicsfirst.spec.js
+++ tests/UIBasicsfirst.spec.js
@@ -1,10 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
 test.only("Browser context First Test case", async ({ browser }) => {
-  const userName = page.locator("#username");
-  const signIn = page.locator("#signInBtn");
-  const password = page.locator("[type='password']");
-
   const context = await browser.newContext();
   const page = await context.newPage();
 
@@ -12,15 +8,15 @@ test.only("Browser context First Test case", async ({ browser }) => {
 
   // css,xpath
 
-  await userName.fill("rahulshettydemy");
-  await password.fill("learning");
-  await signIn.click();
-
+  await page.locator("#username").fill("rahulshettydemy");
+  await page.locator("[type='password']").fill("learninghghg");
+  await page.locator("#signInBtn").click();
   console.log(await page.locator("[style*='block']").textContent());
   await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+});
 
-  // Clear previous value with new one
-  await userName.fill("");
-  await userName.fill("rahulshettyacademy");
-  await signIn.click();
+test("Page context Test case", async ({ page }) => {
+  await page.goto("https://google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google");
 });
