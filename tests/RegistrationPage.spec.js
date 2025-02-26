456143c07943d05e695ca537865425c00b294800
vishalmrode <vishalmrode@outlook.com>
2025-02-26 21:59:45 +0530

    Registration page and capturing the success message

---
+++ tests/RegistrationPage.spec.js
@@ -0,0 +1,56 @@
+const { test, expect } = require("@playwright/test");
+
+test("Registration Page Signup", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // Declaring all the fields as variables for use
+
+  const firstName = page.locator("#firstName");
+  const lastName = page.locator("#lastName");
+  const userEmail = page.locator("#userEmail");
+  const UserMobile = page.locator("#userMobile");
+  const password = page.locator("#userPassword");
+  const confirmPassword = page.locator("#confirmPassword");
+  const checkBox = page.locator("(//input[@type='checkbox'])"); // will use index to get the last value which is checkbox
+  const registerButton = page.locator("#login"); //used class to get the locator
+  const successMessage = page.locator(".headcolor");
+
+  await page.goto("https://rahulshettyacademy.com/client/");
+  await page.waitForTimeout(4000);
+  await page.locator(".text-reset").click();
+  console.log("The Title of page is:", await page.title());
+  await expect(page).toHaveTitle("Let's Shop");
+
+  await firstName.fill("ABCDEFGHIJ");
+  //await page.waitForTimeout(4000);
+  await lastName.fill("testing");
+  await userEmail.fill("test43rrp@gmail.com");
+  await UserMobile.fill("4444444455");
+  await password.fill("Test@000!");
+  await confirmPassword.fill("Test@000!");
+  await checkBox.check();
+  await registerButton.click();
+
+  await page.waitForTimeout(4000);
+  console.log("The success message is:", await successMessage.textContent());
+  await expect(successMessage).toContainText("Account Created Successfully");
+  await page.waitForTimeout(4000);
+});
+
+test.only("Login to the page", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  //   const loginEmail = page.locator("#userEmail");
+  //   const userPassword = page.locator("#userPassword");
+
+  await page.goto("https://rahulshettyacademy.com/client/auth/login");
+
+  console.log("The Title of page is:", await page.title());
+  await expect(page).toHaveTitle("Let's Shop");
+
+  await userEmail.fill("test43rrp@gmail.com");
+  await password.fill("Test@000!");
+  await registerButton.click();
+});
