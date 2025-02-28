fbd00affc4afb8e1f3993ea38d8665b3f2ce368a
vishalmrode <vishalmrode@outlook.com>
2025-02-28 18:03:17 +0530

    Static radio buttons

--- tests/Radiobutton.spec.js
+++ tests/Radiobutton.spec.js
@@ -12,3 +12,20 @@ test("Handle Radio Buttons", async ({ page }) => {
   //negative test case for not checked
   await expect(await page.locator("#gender-female").isChecked()).toBeFalsy(); //isChecked() will return value true and toBeFalsy will verify
 });
+
+test.only("Radio buttons selection", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  const userName = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
+  const password = page.locator("[type='password']");
+  const dropDown = page.locator("select.form-control ");
+
+  await userName.fill("rahulshettyacademy");
+  await password.fill("learning");
+  await page.locator(".radiotextsty").last().click();
+  await page.locator(".btn.btn-success").click();
+  await dropDown.selectOption("consult");
+  await page.waitForTimeout(3000);
+  await signIn.click();
+});
