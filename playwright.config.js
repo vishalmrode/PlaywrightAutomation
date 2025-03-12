0ea1ff7d52350122102e385dc6761453b7b97d35
vishalmrode <vishalmrode@outlook.com>
2025-03-12 22:32:24 +0530

    added some changes

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,18 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["line"], ["html"], ["allure-playwright"]],
+  reporter: [
+    ["line"],
+    ["html"],
+    [
+      "allure-playwright",
+      {
+        detail: true,
+        outputFolder: "allure-results",
+        suiteTitle: false,
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: "chromium",
