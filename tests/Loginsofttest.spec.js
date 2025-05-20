04b55b501d00c3764067957e969e04af35e7f356
vishalmrode <vishalmrode@outlook.com>
2025-05-20 14:37:19 +0530

    Add login test for dummy site using Playwright

---
+++ tests/Loginsofttest.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test("Login to dummy site", { tag: "@smoke" }, async ({ page }) => {
+  await page.goto("https://practicesoftwaretesting.com/");
+  await page.locator('[data-test="nav-sign-in"]').click();
+  await page
+    .locator('[data-test="email"]')
+    .fill("customer@practicesoftwaretesting.com");
+  await page.locator('[data-test="password"]').fill("welcome01");
+  await page.locator('[data-test="login-submit"]').click();
+  await expect(page.locator('[data-test="nav-menu"]')).toContainText(
+    "Jane Doe"
+  );
+});
